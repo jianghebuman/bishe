946df2b430bb54c1eea7033dcd083a4e8dc117c6
jianghebuman <240964258+jianghebuman@users.noreply.github.com>
2026-06-25 21:21:55 +0800

    feat: add login action to login prompt

--- 项目源码/基于SpringBoot框架的校园求职招聘系统设计与实现/frontend/src/utils/loginPrompt.js
+++ 项目源码/基于SpringBoot框架的校园求职招聘系统设计与实现/frontend/src/utils/loginPrompt.js
@@ -1,9 +1,19 @@
+import { h } from 'vue'
 import { ElNotification } from 'element-plus'
 
 export const showLoginPrompt = (message = '请先登录后再使用该功能') => {
-  ElNotification({
+  let notice
+  const goLogin = () => {
+    notice?.close()
+    window.location.href = '/login'
+  }
+
+  notice = ElNotification({
     title: '请先登录',
-    message,
+    message: h('div', { class: 'login-required-content' }, [
+      h('div', { class: 'login-required-text' }, message),
+      h('button', { class: 'login-required-action', type: 'button', onClick: goLogin }, '去登录')
+    ]),
     type: 'warning',
     duration: 6000,
     showClose: true,
